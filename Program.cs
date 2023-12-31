ab1c32bb6e50b07b00232216bbec82f80aa22d55
Vizan Arkonin <altahir911@gmail.com>
2023-12-31 15:01:20 +0300

    + Added programmatic migrations application. Since we only operate local SQLite DB, we have no need for manual migrations management or bundle creation as a standalone entity (at least for now)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿using System.Linq;
-
-using Firewatch.Core;
+﻿using Firewatch.Core;
 using Firewatch.Core.Logging;
 using Firewatch.Database;
-using Firewatch.Database.Models;
+
+using Microsoft.EntityFrameworkCore;
 
 string configFile = "firewatchConfig.xml";
 
@@ -12,3 +11,6 @@ Config config = new Config(configFile, logger.GetLogger("Config"));
 
 FirewatchContext db = new FirewatchContext();
 db.SetLogger(logger.GetLogger("DBContext"));
+/// Since we're only operating the local DB right now - we can use programmatic migrations method to remove an extra-step with
+/// creating and running a bundle prior to application start. We'll assure the DB is up to date on every application start-up
+RelationalDatabaseFacadeExtensions.Migrate(db.Database);
